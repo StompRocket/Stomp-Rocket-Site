03407f34acd42605cd26699ef1e28378183f92e0
Ronan18 <ronan@brainstormincstudio.com>
2018-10-03 21:17:03 -0700

    Icons

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home page">
+    <div>
+      <img alt="Stomp Rocket logo" src="../assets/logo.svg">
+      <h1>STOMP<span>ROCKET</span></h1>
+      <a href="mailto:stomprocketinc@gmail.com">stomprocketinc@gmail.com</a>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'home',
+
+  }
+</script>
