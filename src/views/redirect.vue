e53c2f748250f94fb3e34d1d1f1035e0f1c46ab2
Ronan18 <ronan@brainstormincstudio.com>
2018-10-03 20:32:31 -0700

    Basic home page

---
+++ src/views/redirect.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    loading...
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "redirect",
+    mounted() {
+      let path = this.$route.path;
+      if (path == '/gh') {
+        window.location = 'https://github.com/StompRocket'
+      } else {
+        this.$router.push('/')
+      }
+
+    }
+  }
+</script>
+
