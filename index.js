f138b9158c7c9029752f811a8db35359955446e1
swissChili <sasha@fastmail.com>
2019-03-24 19:00:42 -0700

    Added more projects, changed css

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import '@stomprocket/blastoff'
 import { Navbar } from '@stomprocket/blastoff-preact'
 import { Component } from 'preact'
+import { Router } from 'preact-router'
 import { About } from './about'
 import { Showcase } from './showcase'
-import './style';
+import './style'
 
 let links = [
 	{
@@ -15,29 +16,57 @@ let links = [
 let projects = [
 	{
 		title: "Graphite Writer",
-		desc: "F",
+		desc: "A modren web based text editor, with Firebase, Vue JS, and Quill JS",
 		url: "graphitewriter.com",
-		accent: "graphite"
+		accent: "graphite",
+		repo: "Graphite-Writer"
 	},
 	{
 		title: "Blast Off",
-		desc: "Css Framework",
+		desc: "A minimal and ux-focused CSS framework powered by Flexbox.",
 		url: "blastoff.stomprocket.io",
-		accent: "blastoff"
+		accent: "blastoff",
+		repo: "BlastOff"
+	},
+	{
+		title: "RootEd",
+		desc: "A hackathon project that teaches you vocabulary by making deep connections​ between roots of words.",
+		accent: "rooted",
+		repo: "RootEd"
 	},
 	{
 		title: "Myst",
-		desc: "Elegant weather app",
+		desc: "A a beautiful, lightweight, open source weather app developed by Ronan Furuta",
 		url: "myst.stomprocket.io",
-		accent: "myst"
+		accent: "myst",
+		repo: "Myst"
 	},
 	{
 		title: "Math",
 		desc: "Text based smart calculator",
 		url: "math.stomprocket.io",
-		accent: "math"
+		accent: "math",
+		repo: "Math"
+	},
+	{
+		title: "Hyphen-Hacks",
+		desc: "Internal dashboard for the hyphen-hacks hackathon",
+		url: "hyphen-hacks.com",
+		accent: "hh"
 	}
 ]
+
+class HomePage extends Component {
+	render() {
+		return (
+			<div>
+				<About />
+
+				<Showcase items={projects} />
+			</div>
+		)
+	}
+}
 
 export default class App extends Component {
 	render() {
@@ -45,9 +74,7 @@ export default class App extends Component {
 			<div>
 				<Navbar title={ <img src="assets/sr-logo-light@2x.png" class="nav-img"></img> } links={links} />
 
-				<About />
-
-				<Showcase items={projects} />
+				<HomePage />
 			</div>
 		);
 	}
--- showcase.js
+++ showcase.js
@@ -3,19 +3,40 @@ import { Component } from 'preact'
 export class Showcase extends Component {
     render({ items }) {
         return (
-            <div class="vcenter">
+            <div class="vcenter-full">
                 <div class="container">
-                    <h1 class="bold">Stuff we've made</h1>
+                    <h1 class="bold">Stuff we've worked on</h1>
 
                     <div class="wrap-box">
                         {
                             items.map(i =>
                                 <div class="box container" accent={i.accent}>
-                                    <h2 class="mono">{i.title}</h2>
+                                    
+
+                                    <h2 class="mono">
+                                        {
+                                            i.repo ? 
+                                            <a class="heading-link" href={
+                                                        "//github.com/stomprocket/" + i.repo
+                                                }>
+                                                <img src="assets/github.svg" class="gh-icon" />
+                                            </a>
+                                            : <span />
+                                        }
+
+                                        {i.title}
+                                    </h2>
                                     <p>
                                         {i.desc}
                                     </p>
-                                    <a class="product-link" href={"//" + i.url} target="_blank">{i.url}</a>
+                                    {
+                                        i.url ? (
+                                        <a class="product-link" href={"//" + i.url} target="_blank">
+                                            {i.url}
+                                        </a>
+                                        ) : <i class="product-link">No website available</i>
+                                    }
+                                    <br />
                                 </div>
                             )
                         }
